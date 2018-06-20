e0c8b96c9d35883099772667751605acaabcce16
Alan Barretto <alanbarretto@gmail.com>
2018-06-19 22:02:08 -0700

    feat: Add append to LinkedList

--- solutions.py
+++ solutions.py
@@ -181,16 +181,20 @@ class Node(object):
 class LinkedList(object):
     def __init__(self, head=None):
         self.head = head
-
+    
+    def append(self, new_element):
+        current = self.head
+        if self.head:
+          while current.next:
+            current = current.next
+          current.next = new_element
+        else:
+          self.head = new_element
 
 
 
 
 def question5(ll, m):
-    if not ll:
-        return False
-
-    ll = LinkedList(n)
 
     if not ll.head:
       return None
