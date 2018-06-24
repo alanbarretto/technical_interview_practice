a88770ed0dc12a61a06f0f7a6b0b94d8ad8f0951
Alan Barretto <alanbarretto@gmail.com>
2018-06-24 15:26:11 -0700

    feat: Convert strings to lower case

--- solutions.py
+++ solutions.py
@@ -2,9 +2,6 @@
 """
 
 
-'''My first question is if the string being input will be an English word that can be found in the dictionary, or can it be a string with letters, numbers, ASCII characters, and spaces that the function will need to sort through?
-
-I will assume that it can be anything as long as the data type is a string.  My function will have to check for any character in string form.'''
 
 
 
@@ -17,50 +14,39 @@ def question1(s, t):
     if not t or type(t) != str:
     	return False
 
-    #Remove all spaces from each string.
-
-	s_string = s.replace(" ", "")
-	t_string = t.replace(" ", "")
-
-    #Check if the length of s is not less than the length of t
-
-    if len(s_string) < len(t_string):
-    	return False
-
-    #Convert the strings into a list of the individual letters of the string
-
-    s_list = list(s_string)
-    t_list = list(t_string)
-
     #Initialize empty sets
-
     s_set = set()
     t_set = set()
 
-    #Put the contents of the list into the corresponding sets
+    #Initialize empty dictionaries
+    letter_tally_s = {}
+    letter_tally_t = {}
 
+    #Remove all spaces from each string.  Each is O(n)
+    s_string = s.replace(" ", "").lower()
+    t_string = t.replace(" ", "").lower()
+
+    #Check if the length of s is not less than the length of t
+    if len(s_string) < len(t_string):
+    	return False
+
+    #Convert the strings into a list of the individual letters of the string. Each is O(n)
+    s_list = list(s_string)
+    t_list = list(t_string)
+
+    #Put the contents of the list into the corresponding sets
     for x in s_list:
     	s_set.add(x)
 
     for y in t_list:
     	t_set.add(y)
 
-
-
-    #Check if all characters in t are present in s, otherwise return False
-
+    #Check if all characters in t are present in s, otherwise return False. O(n)
     for letter in t_set:
     	if letter not in s_set:
     		return False
 
-    
-
-    #Initialize empty dictionaries
-
-    letter_tally_s = {}
-    letter_tally_t = {}
-
-    #Tally all letters from each set
+    #Tally all letters from each set. O(n**2)
 
     for letter_t in t_string:
     	count_t = t.count(letter_t)
@@ -70,7 +56,7 @@ def question1(s, t):
     	count_s = s.count(letter_s)
     	letter_tally_s[letter_s] = count_s
 
-    #Check if the letter count is the same for all letters in t as in s
+    #Check if the letter count is the same for all letters in t as in s. O(n)
 
     for letter_t in t_set:
     	if letter_tally_t[letter_t] != letter_tally_s[letter_t]:
