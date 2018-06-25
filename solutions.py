5fcea3ef4072d4d57b5582d5785cd41d8d0b4470
Alan Barretto <alanbarretto@gmail.com>
2018-06-24 21:24:08 -0700

    refactor: Delete insert helper function

--- solutions.py
+++ solutions.py
@@ -299,21 +299,7 @@ class Node(object):
 class BST(object):
     def __init__(self, root):
         self.root = Node(root)
-
-    def insert(self, new_val):
-        self.insert_helper(self.root, new_val)
-
-    def insert_helper(self, current, new_val):
-        if current.value < new_val:
-            if current.right:
-                self.insert_helper(current.right, new_val)
-            else:
-                current.right = Node(new_val)
-        else:
-            if current.left:
-                self.insert_helper(current.left, new_val)
-            else:
-                current.left = Node(new_val)
+    
 
 def unzip(matrix, node):
     temp_node = None
